ce102406a6b1774aaeb45a2bf3db4e51e9f92b3b
Madhan003 <madhansundaram19@gmail.com>
2025-11-28 09:18:47 +0530

    Middleware intro

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // routes declaration
 import userRouter from "./routes/user_route.js";
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter); 
 
 // example route: http://localhost:4000/api/v1/users/register
 export default app;
