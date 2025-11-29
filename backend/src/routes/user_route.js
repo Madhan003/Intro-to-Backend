db02db564f54a7ce1cae949031a0bcbf23af1059
Madhan003 <madhansundaram19@gmail.com>
2025-11-29 12:42:46 +0530

    logout

--- backend/src/routes/user_route.js
+++ backend/src/routes/user_route.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { registerUser,loginUser } from '../controller/user_controller.js';
+import { registerUser,loginUser, logoutuser } from '../controller/user_controller.js';
 
 const router = Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').post(logoutuser);
 
 export default router;
 
